1caa9ef369a55be278743cfad009d7a7bf9a103f
DimazAgastya <90527450+DimazAgastya@users.noreply.github.com>
2022-05-26 11:28:26 +0800

    Add files via upload

--- Components/Form/Form.js
+++ Components/Form/Form.js
@@ -2,12 +2,24 @@ import React, { useRef } from "react";
 import Navbar from "../nav/Navbar";
 import style from "../Form/form.module.css";
 import axios from "axios";
+import { useDropzone } from "react-dropzone";
 
 const Form = () => {
+	// setting input
 	const inputFirstName = useRef();
 	const inputLastName = useRef();
 	const inputLocation = useRef();
 	const inputPhoto = useRef();
+
+	// setting dropzone
+	const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({});
+
+	// setting agar ketika menaruh file muncul nnama
+	const files = acceptedFiles.map((file) => (
+		<li key={file.path}>
+			{file.path} - {file.size} bytes
+		</li>
+	));
 
 	const formSubmitHadler = async (event) => {
 		event.preventDefault();
@@ -36,8 +48,6 @@ const Form = () => {
 		console.log(res.data);
 	};
 
-	// menit ke 54
-
 	return (
 		<>
 			<Navbar />
@@ -57,6 +67,11 @@ const Form = () => {
 				<div className="my-3">
 					<label htmlFor="">Photo</label>
 					<input type="file" name="photo" id="photo" ref={inputPhoto}></input>
+					<div {...getRootProps()}>
+						<input {...getInputProps()} />
+						{isDragActive ? <p>Drop the files here ...</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
+					</div>
+					<ul>{files}</ul>
 				</div>
 				<div>
 					<input type="submit" value="SUBMIT FORM" className={`${style.buttons} mt-4`} />
